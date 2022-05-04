625e034f43c20739eaf4d62ed7fbba83d9bd2f9e
Bartosz <bartoszsdr@gmail.com>
2022-05-04 14:50:15 +0200

    Initial commit

--- 01-list-filter/main.js
+++ 01-list-filter/main.js
@@ -1,3 +1,5 @@
+// Standard method
+
 const search = document.querySelector('.search')
 const li = document.querySelectorAll('li')
 
@@ -9,6 +11,23 @@ const searchEngine = e => {
 			el.style.display = 'block'
 		} else {
 			el.style.display = 'none'
+		}
+	})
+}
+
+search.addEventListener('keyup', searchEngine)
+
+// RegExp method
+
+const search = document.querySelector('.search')
+const liItems = document.querySelectorAll('li')
+const searchEngine = e => {
+	liItems.forEach(item => {
+		const match = new RegExp(search.value, 'i').test(item.textContent)
+		if (!match) {
+			item.style.display = 'none'
+		} else {
+			item.style.display = 'block'
 		}
 	})
 }
